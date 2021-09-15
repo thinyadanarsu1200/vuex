1ceb46b9fbca450699dd389e8af32793f7badad5
thinyadanarsu <thinyadanarsu1200@gmail.com>
2021-09-15 15:10:35 +0630

    initial commit

---
+++ src/store/modules/todos.js
@@ -0,0 +1,9 @@
+export default{
+    namespaced: true,
+    state:{
+        greeting: 'Hola Guys!',
+    },
+    getters:{},
+    mutations:{},
+    actions:{},
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <h1>{{this.greeting}}</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import {mapState} from 'vuex';
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+ name: 'Home',
+ computed:{
+      ...mapState('Todos',['greeting']),
+ }
+
 }
 </script>
